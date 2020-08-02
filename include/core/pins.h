6376510c07795e2fa45e40b5ce6112177285c0e2
Zach Lambert <zachalambert@gmail.com>
2020-08-02 13:15:50 +0100

    Changed the Pin datatype

--- include/core/pins.h
+++ include/core/pins.h
@@ -3,51 +3,38 @@
 
 #include <stdint.h>
 
-typedef enum {
-    IOPort_B,
-    IOPort_C,
-    IOPort_D,
-    IOPort_COUNT
-} IOPort;
+// Use an opaque handle to pin data
+typedef void *Pin;
 
-typedef struct {
-    volatile uint8_t *direction_reg; // DDRX
-    volatile uint8_t *output_reg; // PORTX
-    volatile uint8_t *input_reg; // PINX
-    uint8_t bit;
-} IOPin;
+void gpio_mode_input(Pin pin);
+void gpio_mode_input_pullup(Pin pin);
+void gpio_mode_output(Pin pin);
+void gpio_mode_output_inverse(Pin pin);
 
-IOPin make_io_pin(IOPort port, uint8_t bit);
+void gpio_write(Pin pin, uint8_t value);
+uint8_t gpio_read(Pin pin);
 
-void gpio_mode_input(IOPin pin);
-void gpio_mode_input_pullup(IOPin pin);
-void gpio_mode_output(IOPin pin);
-void gpio_mode_output_inverse(IOPin pin);
-
-void gpio_write(IOPin pin, uint8_t value);
-uint8_t gpio_read(IOPin pin);
-
-extern const IOPin PIN_D0;
-extern const IOPin PIN_D1;
-extern const IOPin PIN_D2;
-extern const IOPin PIN_D3;
-extern const IOPin PIN_D4;
-extern const IOPin PIN_D5;
-extern const IOPin PIN_D6;
-extern const IOPin PIN_D7;
-extern const IOPin PIN_D8;
-extern const IOPin PIN_D9;
-extern const IOPin PIN_D10;
-extern const IOPin PIN_D11;
-extern const IOPin PIN_D12;
-extern const IOPin PIN_D13;
-extern const IOPin PIN_A0;
-extern const IOPin PIN_A1;
-extern const IOPin PIN_A2;
-extern const IOPin PIN_A3;
-extern const IOPin PIN_A4;
-extern const IOPin PIN_A5;
-extern const IOPin PIN_BUILT_IN_LED;
+extern const Pin PIN_D0;
+extern const Pin PIN_D1;
+extern const Pin PIN_D2;
+extern const Pin PIN_D3;
+extern const Pin PIN_D4;
+extern const Pin PIN_D5;
+extern const Pin PIN_D6;
+extern const Pin PIN_D7;
+extern const Pin PIN_D8;
+extern const Pin PIN_D9;
+extern const Pin PIN_D10;
+extern const Pin PIN_D11;
+extern const Pin PIN_D12;
+extern const Pin PIN_D13;
+extern const Pin PIN_A0;
+extern const Pin PIN_A1;
+extern const Pin PIN_A2;
+extern const Pin PIN_A3;
+extern const Pin PIN_A4;
+extern const Pin PIN_A5;
+extern const Pin PIN_BUILT_IN_LED;
 
 #define PIN_BUILT_IN_LED PIN_D13
 #define PIN_SS PIN_D10
--- src/core/pins.c
+++ src/core/pins.c
@@ -4,194 +4,218 @@
 
 #include "core/regs.h"
 
-// To use the GPIO pins, you need to write to the relevant
-// registers to configure the direction (input / output) and
-// write data (write to the PORTX registers), or read data
-// (read from the PINX register).
-// These registers are mapped to specific regions in memory,
-// identified with DDRA, DDRB, etc.
-// To write to the registers, write to these specific pieces
-// of memory, using pointers
 
-volatile uint8_t *const io_direction_regs[] = {
-    &DDRB, &DDRC, &DDRD
-};
-volatile uint8_t *const io_output_regs[] = {
-    &PORTB, &PORTC, &PORTD
-};
-volatile uint8_t *const io_input_regs[] = {
-    &PINB, &PINC, &PIND
-};
+typedef struct {
+    volatile uint8_t *direction_reg; // DDRX
+    volatile uint8_t *output_reg; // PORTX
+    volatile uint8_t *input_reg; // PINX
+    uint8_t bit;
+    uint8_t interrupt_num;
+} PinData;
 
-IOPin make_io_pin(IOPort port, uint8_t bit)
+
+void gpio_mode_input(Pin pin)
 {
-    IOPin pin = {
-        .direction_reg = io_direction_regs[port],
-        .output_reg = io_output_regs[port],
-        .input_reg = io_input_regs[port],
-        .bit = bit
-    };
-    return pin;
+    PinData *data = (PinData*)pin;
+    reg_write_bit(data->output_reg, data->bit, 0);
+    reg_write_bit(data->direction_reg, data->bit, 0);
 }
 
-void gpio_mode_input(IOPin pin)
+void gpio_mode_input_pullup(Pin pin)
 {
-    reg_write_bit(pin.output_reg, pin.bit, 0);
-    reg_write_bit(pin.direction_reg, pin.bit, 0);
-}
-
-void gpio_mode_input_pullup(IOPin pin)
-{
+    PinData *data = (PinData*)pin;
     // Disabled if PUD flag is set (pull-up disable)
-    reg_write_bit(pin.output_reg, pin.bit, 1);
-    reg_write_bit(pin.direction_reg, pin.bit, 0);
+    reg_write_bit(data->output_reg, data->bit, 1);
+    reg_write_bit(data->direction_reg, data->bit, 0);
 }
 
-void gpio_mode_output(IOPin pin)
+void gpio_mode_output(Pin pin)
 {
+    PinData *data = (PinData*)pin;
     // Sourcing input
     // When the bit is set, the output is pulled high
-    reg_write_bit(pin.output_reg, pin.bit, 1);
-    reg_write_bit(pin.direction_reg, pin.bit, 1);
+    reg_write_bit(data->output_reg, data->bit, 1);
+    reg_write_bit(data->direction_reg, data->bit, 1);
 }
 
-void gpio_mode_output_inverse(IOPin pin)
+void gpio_mode_output_inverse(Pin pin)
 {
+    PinData *data = (PinData*)pin;
     // Sinking input
     // When the bit is set, the output is pulled low
-    reg_write_bit(pin.output_reg, pin.bit, 0);
-    reg_write_bit(pin.direction_reg, pin.bit, 1);
+    reg_write_bit(data->output_reg, data->bit, 0);
+    reg_write_bit(data->direction_reg, data->bit, 1);
 }
 
-void gpio_write(IOPin pin, uint8_t value)
+void gpio_write(Pin pin, uint8_t value)
 {
-    reg_write_bit(pin.output_reg, pin.bit, value);
+    PinData *data = (PinData*)pin;
+    reg_write_bit(data->output_reg, data->bit, value);
 }
 
-uint8_t gpio_read(IOPin pin)
+uint8_t gpio_read(Pin pin)
 {
-    return reg_read_bit(pin.input_reg, pin.bit);
+    PinData *data = (PinData*)pin;
+    return reg_read_bit(data->input_reg, data->bit);
 }
 
 // TODO Put all the pins here
-const IOPin PIN_D0 = {
+PinData PIN_D0_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 0
 };
-const IOPin PIN_D1 = {
+const Pin PIN_D0 = &PIN_D0_data;
+
+PinData PIN_D1_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 1
 };
-const IOPin PIN_D2 = {
+const Pin PIN_D1 = &PIN_D1_data;
+
+PinData PIN_D2_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 2
 };
-const IOPin PIN_D3 = {
+const Pin PIN_D2 = &PIN_D2_data;
+
+PinData PIN_D3_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 3
 };
-const IOPin PIN_D4 = {
+const Pin PIN_D3 = &PIN_D3_data;
+
+PinData PIN_D4_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 4
 };
-const IOPin PIN_D5 = {
+const Pin PIN_D4 = &PIN_D4_data;
+
+PinData PIN_D5_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 5
 };
-const IOPin PIN_D6 = {
+const Pin PIN_D5 = &PIN_D5_data;
+
+PinData PIN_D6_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 6
 };
-const IOPin PIN_D7 = {
+const Pin PIN_D6 = &PIN_D6_data;
+
+PinData PIN_D7_data = {
     .direction_reg = &DDRD,
     .output_reg = &PORTD,
     .input_reg = &PIND,
     .bit = 7
 };
-const IOPin PIN_D8 = {
-    .direction_reg = &DDRB,
-    .output_reg = &PORTB,
-    .input_reg = &PINB,
-    .bit = 0
-};
-const IOPin PIN_D9 = {
-    .direction_reg = &DDRB,
-    .output_reg = &PORTB,
-    .input_reg = &PINB,
-    .bit = 1
-};
-const IOPin PIN_D10 = {
-    .direction_reg = &DDRB,
-    .output_reg = &PORTB,
-    .input_reg = &PINB,
-    .bit = 2
-};
-const IOPin PIN_D11 = {
-    .direction_reg = &DDRB,
-    .output_reg = &PORTB,
-    .input_reg = &PINB,
-    .bit = 3
-};
-const IOPin PIN_D12 = {
-    .direction_reg = &DDRB,
-    .output_reg = &PORTB,
-    .input_reg = &PINB,
-    .bit = 4
-};
-const IOPin PIN_D13 = {
-    .direction_reg = &DDRB,
-    .output_reg = &PORTB,
-    .input_reg = &PINB,
-    .bit = 5
-};
+const Pin PIN_D7 = &PIN_D7_data;
 
-const IOPin PIN_A0 = {
+PinData PIN_D8_data = {
+    .direction_reg = &DDRB,
+    .output_reg = &PORTB,
+    .input_reg = &PINB,
+    .bit = 0
+};
+const Pin PIN_D8 = &PIN_D8_data;
+
+PinData PIN_D9_data = {
+    .direction_reg = &DDRB,
+    .output_reg = &PORTB,
+    .input_reg = &PINB,
+    .bit = 1
+};
+const Pin PIN_D9 = &PIN_D9_data;
+
+PinData PIN_D10_data = {
+    .direction_reg = &DDRB,
+    .output_reg = &PORTB,
+    .input_reg = &PINB,
+    .bit = 2
+};
+const Pin PIN_D10 = &PIN_D10_data;
+
+PinData PIN_D11_data = {
+    .direction_reg = &DDRB,
+    .output_reg = &PORTB,
+    .input_reg = &PINB,
+    .bit = 3
+};
+const Pin PIN_D11 = &PIN_D11_data;
+
+PinData PIN_D12_data = {
+    .direction_reg = &DDRB,
+    .output_reg = &PORTB,
+    .input_reg = &PINB,
+    .bit = 4
+};
+const Pin PIN_D12 = &PIN_D12_data;
+
+PinData PIN_D13_data = {
+    .direction_reg = &DDRB,
+    .output_reg = &PORTB,
+    .input_reg = &PINB,
+    .bit = 5
+};
+const Pin PIN_D13 = &PIN_D13_data;
+
+PinData PIN_A0_data = {
     .direction_reg = &DDRC,
     .output_reg = &PORTC,
     .input_reg = &PINC,
     .bit = 0
 };
-const IOPin PIN_A1 = {
+const Pin PIN_A0 = &PIN_A0_data;
+
+PinData PIN_A1_data = {
     .direction_reg = &DDRC,
     .output_reg = &PORTC,
     .input_reg = &PINC,
     .bit = 1
 };
-const IOPin PIN_A2 = {
+const Pin PIN_A1 = &PIN_A1_data;
+k
+PinData PIN_A2_data = {
     .direction_reg = &DDRC,
     .output_reg = &PORTC,
     .input_reg = &PINC,
     .bit = 2
 };
-const IOPin PIN_A3 = {
+const Pin PIN_A2 = &PIN_A2_data;
+
+PinData PIN_A3_data = {
     .direction_reg = &DDRC,
     .output_reg = &PORTC,
     .input_reg = &PINC,
     .bit = 3
 };
-const IOPin PIN_A4 = {
+const Pin PIN_A3 = &PIN_A3_data;
+
+PinData PIN_A4_data = {
     .direction_reg = &DDRC,
     .output_reg = &PORTC,
     .input_reg = &PINC,
     .bit = 4
 };
-const IOPin PIN_A5 = {
+const Pin PIN_A4 = &PIN_A4_data;
+
+PinData PIN_A5_data = {
     .direction_reg = &DDRC,
     .output_reg = &PORTC,
     .input_reg = &PINC,
     .bit = 5
 };
+const Pin PIN_A5 = &PIN_A5_data;
